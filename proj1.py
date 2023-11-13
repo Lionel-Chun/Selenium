333e69ce34b8528ad9b8dc6e8e6985ffe7f2fa93
Lionel Chun <lionel.chun@outlook.com>
2023-11-13 14:43:36 +0800

    beta 2

--- proj1.py
+++ proj1.py
@@ -1,7 +1,11 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+import time
 
-service_obj = Service();
+service_obj = Service()
 driver = webdriver.Chrome(service=service_obj)
 url = "http://www.google.com"
 driver.get(url)
+driver.maximize_window()
+time.sleep(3)
+driver.close()

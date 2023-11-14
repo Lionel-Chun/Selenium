cba882c63ecffe0f34cddec1239bb8bbabab9d1f
Lionel Chun <lionel.chun@outlook.com>
2023-11-14 15:43:51 +0800

    ...

---
+++ Test.py
@@ -0,0 +1,8 @@
+def call(x):
+    print(x)
+
+def call(x, y):
+    print(x * y)
+
+call(5)
+call(5, 2)
--- proj1.py
+++ proj1.py
@@ -1,11 +1,22 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 import time
 
 service_obj = Service()
 driver = webdriver.Chrome(service=service_obj)
-url = "http://www.google.com"
+# url = "https://www.baidu.com/"
+# value = "kw"
+# url = "https://www.google.com/"
+# value = "ApjFqb"
+url = "https://hk.yahoo.com/"
+value = "p"
 driver.get(url)
 driver.maximize_window()
-time.sleep(3)
+search = driver.find_element(By.NAME, value)
+search.send_keys('erb')
+search.send_keys(Keys.ENTER)
+time.sleep(300)
+search.clear()
 driver.close()
